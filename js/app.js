7a3e27662be5ab46cf00b53adfe9e0747c64e731
YaroslavBorshevsky812 <Speransky812@mail.ru>
2021-10-30 18:45:05 +0300

    add moment

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const hours = document.getElementById('hours')
 const minutes = document.getElementById('minutes')
 const seconds = document.getElementById('seconds')
 
+
 function getDateNow() {
     setInterval(
         () => {
